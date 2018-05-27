200a7fb16df23b0e0155b903a623691ae7a66632
dimashyshkin <dimashyshkin@hotmail.com>
2018-05-27 17:56:10 -0400

    bat files

---
+++ src/main/resources/start_hub.bat
@@ -0,0 +1,4 @@
+c:
+cd c:\Users\Dee\SeleniumServer\
+java -jar selenium-server-standalone-3.11.0.jar -role hub
+pause
---
+++ src/main/resources/start_node.bat
@@ -0,0 +1,4 @@
+c:
+cd c:\Users\Dee\SeleniumServer\
+java -jar selenium-server-standalone-3.11.0.jar -role node -hub http://192.168.0.2:4444/grid/register/ -browser browserName=firefox
+pause
